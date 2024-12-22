673b295d35d9f488a36e9fb5822926592a4d92e8
bmchien1 <chienbui10052003@gmail.com>
2024-12-22 13:30:20 +0700

    ui and api update

--- app/src/main/java/com/example/locker/ui/AddCardActivity.kt
+++ app/src/main/java/com/example/locker/ui/AddCardActivity.kt
@@ -5,11 +5,14 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.example.locker.R
 import com.example.locker.api.ApiClient
 import com.example.locker.models.RegisterUserRequest
 import com.example.locker.models.SimpleResponse
 import com.example.locker.utils.TokenManager
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -22,7 +25,6 @@ class AddCardActivity : AppCompatActivity() {
         val fullNameEditText = findViewById<EditText>(R.id.fullNameEditText)
         val birthdayEditText = findViewById<EditText>(R.id.birthdayEditText)
         val phoneEditText = findViewById<EditText>(R.id.phoneEditText)
-        val idCardEditText = findViewById<EditText>(R.id.idCardEditText)
         val addButton = findViewById<Button>(R.id.addButton)
 
         val tokenManager = TokenManager(this)
@@ -32,8 +34,7 @@ class AddCardActivity : AppCompatActivity() {
             val fullName = fullNameEditText.text.toString().trim()
             val birthday = birthdayEditText.text.toString().trim()
             val phone = phoneEditText.text.toString().trim()
-            val idCard = idCardEditText.text.toString().trim()
-
+            val idCard = "123456789"
             if (token != null && fullName.isNotEmpty() && birthday.isNotEmpty() && phone.isNotEmpty() && idCard.isNotEmpty()) {
                 // Register user
                 val registerUserRequest = RegisterUserRequest(fullName, birthday, phone)
@@ -44,8 +45,12 @@ class AddCardActivity : AppCompatActivity() {
                             response: Response<SimpleResponse>
                         ) {
                             if (response.isSuccessful) {
-                                // Check card after registering
-                                checkCard(idCard)
+                                Toast.makeText(this@AddCardActivity, "User registered successfully. Please swipe your card.", Toast.LENGTH_SHORT).show()
+
+                                lifecycleScope.launch {
+                                    delay(6000)  // Wait for 6 seconds
+                                    checkCard(idCard)  // Replace idCard with the actual ID you want to check
+                                }
                             } else {
                                 Toast.makeText(this@AddCardActivity, "Failed to register user", Toast.LENGTH_SHORT).show()
                             }
